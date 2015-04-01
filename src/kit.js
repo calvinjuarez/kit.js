d4e42638ac2813b9e4cd20c90fe5217195510f7f
Calvin Juárez <calvin.juarez@gmail.com>
2015-03-31 20:09:45 -0600

    src – layout how to handle options

--- src/kit.js
+++ src/kit.js
@@ -10,9 +10,19 @@
 
 (function (window, undefined) {
 	
+	var defaults = {
+		debug: false
+	}
+	
+	
 	//! Constructor
 	
 	var LPEngineKIT = function (src, options) {
+		
+		// set properties
+		this.options = prepareOptions(options)
+		
+		// init
 		return this.init()
 	}
 	
@@ -30,5 +40,25 @@
 	
 	
 	//! Private Methods
+	
+	//! -- Utilities
+	
+	function prepareOptions(options) {
+		if (!options) return defaults // JIC, really.  Might should error instead.
+		
+		var option
+		
+		// cleanse options of useless properties
+		for (option in options)                                                                           // for each passed option,
+			if (Object.prototype.hasOwnProperty.call(options, option) && !defaults.hasOwnProperty(option)) //    if it exists in `options` but _not_ in `defaults`,
+				delete options[option]                                                                      //    dump it.
+		
+		// merge defaults into options, favoring options
+		for (option in defaults)                                                                          // for each available option,
+			if (defaults.hasOwnProperty(option) && !Object.prototype.hasOwnProperty.call(options, option)) //    if it exists in `defaults` but _not_ in `options`,
+				options[option] = defaults[option]                                                          //    let `options` have the default option.
+		
+		return options
+	}
 	
 })(window)
